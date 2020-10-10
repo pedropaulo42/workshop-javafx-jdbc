ca6f91067bb37972a5c08d9aa1b523634def7100
Pedro Paulo <pedroppaulo42@gmail.com>
2020-10-10 17:38:18 -0300

    Remove department

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -84,15 +85,13 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 		PreparedStatement st = null;
 		
 		try {
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE Id = ?");
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
 			
 			st.setInt(1, id);
 			st.executeUpdate();
 		}
 		catch(SQLException e) {
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
